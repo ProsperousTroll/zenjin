90e2006e02af5e8f35cdb3e28aae477608f05948
ProsperousTroll <ivekilled4dogs@gmail.com>
2025-11-13 02:58:17 -0800

    patch: fix brain damage

--- include/zenjin.hpp
+++ include/zenjin.hpp
@@ -1,8 +1,54 @@
 #pragma once
 #include <iostream>
-#include "raylib.h"
-#include "states.hpp"
 #include "config.hpp"
+#include "raylib.h"
+
+namespace States {
+   class EmptyState{
+      public:
+         virtual void init(){}
+         virtual void update(){}
+         virtual void draw(){}
+         virtual void unload(){}
+   };
+
+   class Manager {
+         Manager(){};
+         inline static Manager* man{nullptr};
+
+      public:
+         EmptyState* currentState;
+         static Manager* initManager() {
+            if(!man){
+               man = new Manager();
+            }
+            return man;
+         }
+
+         EmptyState* getState(){
+            return currentState;
+         }
+         
+         void setState(EmptyState* s){
+            currentState = s;
+         }
+   };
+
+   class TitleScreen : public EmptyState {
+      public:
+         void update() override;
+         void draw() override;
+   };
+
+   class MainGame : public EmptyState {
+      public:
+         void update() override;
+         void draw() override;
+   };
+}
+
+inline States::TitleScreen title;
+inline States::MainGame game;
 
 namespace Zenjin{
    inline void Log(const char* input){
@@ -46,3 +92,52 @@ namespace Zenjin{
          }
    };
 }
+
+namespace Entity {
+   // tag enum for different object types.
+   enum Tag {
+      PLAYER, // a player controller
+      NPC, // any kind of enemy, friend, or hostile
+      OBJECT, // an interactable part of the world
+      ITEM, // something that can be equipped
+      WORLD, // world geometry and such
+      UI, // what do you think?
+   };
+
+   // blank template for all game objects and entities.
+   class Object {
+      public:
+         Vector2 pos;
+         Rectangle bounds;
+         Texture2D sprite;
+
+         virtual void init(){}
+         virtual void update(){}
+         virtual void draw(){}
+         virtual void unload(){}
+   };
+
+   class Player : public Object {
+      public:
+         Player(){
+            bounds.x = 64;
+            bounds.y = 128;
+            pos.x = WINWIDTH / 2 - bounds.x;
+            pos.y = WINHEIGHT / 2 - bounds.y;
+         }
+         Vector2 vel;
+
+         void move();
+
+         void update() override;
+         void draw() override;
+   };
+}
+
+/*
+inline std::unordered_map<Entity::Tag, Entity::Object> World{
+ {Entity::PLAYER, Entity::Player()},
+};
+*/
+
+
--- src/states.cpp
+++ src/states.cpp
@@ -1,4 +1,4 @@
-#include "states.hpp"
+#include "zenjin.hpp"
 #include "config.hpp"
 #include <raylib.h>
 
@@ -14,28 +14,14 @@ void States::TitleScreen::draw(){
    DrawText("WELCOME TO GAME", WINWIDTH / 2, WINHEIGHT / 2, 32, BLACK);
 }
 
-Vector2 recPos{0, WINHEIGHT / 2 - 64};
-int inputDir{0};
-float speed{0};
-float maxSpeed{4};
-float accel{0.3};
-
+// MAIN GAME
 void States::MainGame::update(){
    if(IsKeyPressed(KEY_SPACE)){
       States::Manager::initManager()->setState(&title);
    }
-
-   inputDir = IsKeyDown(KEY_D) - IsKeyDown(KEY_A);
-   if((inputDir != 0) && (speed < maxSpeed)){
-      speed += accel;
-   } else if(inputDir == 0) {
-      speed = 0;
-   }
-   recPos.x += inputDir * speed;
 }
 
 void States::MainGame::draw(){
    ClearBackground(GRAY);
    DrawFPS(WINWIDTH / 36, WINHEIGHT / 36);
-   DrawRectangle(recPos.x, recPos.y, 64, 128, YELLOW);
 }
